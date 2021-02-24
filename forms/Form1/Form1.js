054010ea2c7d791ef3c20dc40b17c5c268b9eb3b
arjun1499 <ahs18437@creighton.edu>
2021-02-24 16:22:58 -0600

    Change Finals

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,17 +1,15 @@
-//c
 let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
- //d
+ 
 let withSal = petNames
 withSal = petNames
+
 withSal.push("sal")
-//e
-console.log(withSal)
- //f question asks for "petsArray" (assumed "withSal" array)
+ 
  let thirdPet = withSal[2]
 console.log(`The third pet is named ${thirdPet}.`)
-console.log(`The third pet is named ${thirdPet}.`)
-//g
-console.log(`The array has ${withSal.length} names in it.`)
-//h
+
+let length = withSal.length
+console.log(`The array has ${length} names in it.`)
+
 withSal.pop(10)
-console.log(withSal)
+
